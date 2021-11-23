374c53825723ce36ecc29707f59cfb0fe5830d90
Damienvd <70859110+Damienvd@users.noreply.github.com>
2021-11-23 09:00:23 +0200

    Add files via upload

---
+++ CMPG323.P2.Services.BusinessLogic/AuthenticationService.cs
@@ -0,0 +1,141 @@
+﻿using CMPG323.P2.Services.DataAccess;
+using CMPG323.P2.Services.Library.Models.Domain;
+using CMPG323.P2.Services.Library.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPG323.P2.Services.BusinessLogic
+{
+
+    #region Interface
+
+    public interface IAuthenticationService
+    {
+        Task<string> AuthenticateApplicationUser(LoginDetail loginDetail);
+        Task<int?> AuthorizeUser(string accessToken);
+    }
+
+    #endregion
+
+    public class AuthenticationService : IAuthenticationService
+    {
+
+        #region Private Fields
+
+        private readonly string _jwtSecretKey;
+
+        private readonly ImageContext _db;
+
+        #endregion
+
+        #region Constructors
+
+        public AuthenticationService(IConfiguration configuration, ImageContext db)
+        {
+            _jwtSecretKey = configuration["AppSettings:JwtSecretKey"];
+            _db = db;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public async Task<string> AuthenticateApplicationUser(LoginDetail loginDetail)
+        {
+            User user = await AuthenticateUser(loginDetail);
+            return GenerateJwtToken(user.Username);
+        }
+
+        public async Task<int?> AuthorizeUser(string accessToken)
+        {
+            try
+            {
+                string userName = ValidateJwtToken(accessToken);
+                User user = await _db.Users.AsNoTracking().SingleOrDefaultAsync(e => e.Username.ToUpper().Equals(userName.ToUpper()));
+                return user?.Id;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private async Task<User> AuthenticateUser(LoginDetail loginDetail)
+        {
+            User user = await _db.Users.SingleOrDefaultAsync(e => e.Username.ToUpper().Equals(loginDetail.Username.ToUpper()));
+
+            if (user != null)
+            {
+                if (loginDetail.Password.Equals(user.Password))
+                {
+                    await _db.SaveChangesAsync();
+                    return user;
+                }
+                else
+                {
+                    throw new Exception("Invalid password");
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid username");
+            }
+        }
+
+        private string ValidateJwtToken(string accessToken)
+        {
+            if (string.IsNullOrWhiteSpace(accessToken))
+                throw new Exception("We could not resolve your authentication request.");
+
+            if (accessToken.StartsWith("Bearer "))
+            {
+                accessToken = accessToken["Bearer ".Length..].Trim();
+            }
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                ValidateLifetime = true,
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwtSecretKey))
+            };
+            handler.ValidateToken(accessToken, validationParameters, out SecurityToken securityToken);
+
+            return ((JwtSecurityToken)securityToken).Claims.First(e => e.Type == "unique_name").Value;
+        }
+
+        private string GenerateJwtToken(string userName)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(_jwtSecretKey);
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, userName)
+                }),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Expires = DateTime.Now.AddHours(8)
+            };
+
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        #endregion
+
+    }
+}
---
+++ CMPG323.P2.Services.BusinessLogic/FileService.cs
@@ -0,0 +1,146 @@
+﻿using CMPG323.P2.Services.DataAccess;
+using CMPG323.P2.Services.Library.Models.Domain;
+using CMPG323.P2.Services.Library.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPG323.P2.Services.BusinessLogic
+{
+
+    #region DI Interface
+
+    public interface IFileService
+    {
+        Task<int> CreateFile(FileItemCreateDetail fileItemCreateDetail);
+        Task<FileStreamResult> DownloadFile(string fileGuid);
+        Task DeleteFile(int fileItemId);
+    }
+
+    #endregion
+
+    public class FileService : IFileService
+    {
+
+        #region Private Fields
+
+        private readonly ImageContext _db;
+
+        private readonly string _filesDirectory;
+
+        #endregion
+
+        #region Constructors
+
+        public FileService(ImageContext db, IConfiguration configuration)
+        {
+            _db = db;
+            _filesDirectory = configuration.GetConnectionString("ImageFiles");
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public async Task<int> CreateFile(FileItemCreateDetail fileCreateDetail)
+        {
+            if (fileCreateDetail.File != null)
+            {
+                string path = Path.Combine(_filesDirectory, fileCreateDetail.File.FileName);
+                string displayName = Path.GetFileNameWithoutExtension(path);
+                string fileExtension = Path.GetExtension(path);
+
+                #region Save Details to Database
+
+                /*
+                 * Create FileItem database entry first, as we use the identity 
+                 * inserted Id to rename the file name before uploading.
+                 */
+                FileItem fileItem = new()
+                {
+                    DisplayName = displayName,
+                    ContentType = fileCreateDetail.File.ContentType,
+                    Size = fileCreateDetail.File.Length,
+                    Extension = fileExtension,
+                    Guid = Guid.NewGuid().ToString(),
+                    UserId = fileCreateDetail.UserId,
+                    DateCaptured = DateTime.Now,
+                    Geolocation = fileCreateDetail.Geolocation,
+                    Tags = fileCreateDetail.Tags
+                };
+                _db.Add(fileItem);
+                await _db.SaveChangesAsync();
+
+                #endregion
+
+                #region Upload File
+
+                string fileName = $"{fileItem.Id}{fileExtension}"; // FileItem Id as file name to ensure uniqueness.
+                string filePath = Path.Combine(_filesDirectory, fileName);
+                using (FileStream fileStream = File.Create(filePath))
+                {
+                    Stream inputStream = fileCreateDetail.File.OpenReadStream();
+                    await inputStream.CopyToAsync(fileStream);
+                }
+
+                #endregion
+
+                _db.Entry(fileItem).State = EntityState.Detached;
+                return fileItem.Id;
+            }
+            else
+            {
+                throw new Exception("No file specified.");
+            }
+        }
+
+        public async Task<FileStreamResult> DownloadFile(string fileGuid)
+        {
+            FileItem fileItem = await _db.FileItems.AsNoTracking().SingleOrDefaultAsync(e => e.Guid.Equals(fileGuid)) ??
+                throw new Exception("File not found");
+
+            try
+            {
+                FileStream fileStream = File.OpenRead(GetFilePath(fileItem));
+                return new FileStreamResult(fileStream, fileItem.ContentType)
+                {
+                    FileDownloadName = $"{fileItem.DisplayName}{fileItem.Extension}"
+                };
+            }
+            catch (FileNotFoundException)
+            {
+                throw new Exception("File not found");
+            }
+        }
+
+        public async Task DeleteFile(int fileItemId)
+        {
+            FileItem fileItem = await _db.FileItems.SingleOrDefaultAsync(e => e.Id == fileItemId) ??
+                throw new Exception("File not found");
+
+            _db.Remove(fileItem);
+            await _db.SaveChangesAsync();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private string GetFilePath(FileItem fileItem)
+        {
+            string fileName = $"{fileItem.Id}{fileItem.Extension}";
+            return Path.Combine(_filesDirectory, fileName);
+        }
+
+        #endregion
+
+
+    }
+}
